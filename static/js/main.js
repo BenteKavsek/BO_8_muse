8956bcff96c76924b741745b552e220ac65d685d
BenteKavsek <35726@ma-web.nl>
2024-06-10 12:01:39 +0200

    goede text komt op op klikbare text

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,88 @@
 const popups = document.getElementById("parent__popup");
 
 const schip_button = document.getElementById("schip_button");
-const schip_card = document.getElementById("schip-t-wapen");
-const amsterdam_button = document.getElementById("amsterdam__button");
+const schip_card = document.getElementById("schip_wapen");
+
+const amsterdam_button = document.getElementById("amsterdam_button");
 const amsterdam_card = document.getElementById("amsterdam");
-const mau__button = document.getElementById("mauritius");
-const mau = document.getElementById("rivier-mauritius");
+
+const mau_button = document.getElementById("mauritius_button");
+const mau_card = document.getElementById("mau_card");
+
+const eiland_button = document.getElementById("eiland_button");
+const eiland_card = document.getElementById("eiland_card");
+
+const wilden_button = document.getElementById("wilden_button");
+const wilden_card = document.getElementById("wilden_card");
+
+const gulden_button = document.getElementById("gulden_button");
+const gulden_card = document.getElementById("gulden_card");
+
+const lading_button = document.getElementById("lading_button");
+const lading_card = document.getElementById("lading_card");
+
+const ossenhuid_button = document.getElementById("ossenhuid_button");
+const ossenhuid_card = document.getElementById("ossenhuid_card");
+
 
 function clearall() {
+    schip_card.style.display = ""
+    amsterdam_card.style.display = ""
+    mau_card.style.display = ""
+    eiland_card.style.display = ""
+    wilden_card.style.display = ""
+    gulden_card.style.display = ""
+    lading_card.style.display = ""
+    ossenhuid_card.style.display = ""
+};
 
-}
+clearall();
 
 schip_button.addEventListener("click", function () {
-    console.log("foo");
-    popups.style.display = "flex"
-    schip_card.style.display = "flex"
-    amsterdam.style.display = ""
-    mau.style.display = ""
+    clearall();
+    schip_card.style.display = "flex";
 })
-
 
 amsterdam_button.addEventListener("click", function () {
-    console.log("foo1");
-    popups.style.display = "flex"
-    amsterdam_card.style.display = "flex"
-    schip_card.style.display = ""
-    mau.style.display = ""
-    //Hier moet volgorde display nog goed
+    clearall()
+    amsterdam_card.style.display = "flex";
+})
 
+mau_button.addEventListener("click", function () {
+    clearall()
+    mau_card.style.display = "flex";
+})
+
+eiland_button.addEventListener("click", function () {
+    clearall()
+    eiland_card.style.display = "flex";
+})
+
+wilden_button.addEventListener("click", function () {
+    clearall()
+    wilden_card.style.display = "flex";
+})
+
+gulden_button.addEventListener("click", function () {
+    clearall()
+    gulden_card.style.display = "flex";
+})
+
+lading_button.addEventListener("click", function () {
+    clearall()
+    lading_card.style.display = "flex";
+})
+
+ossenhuid_button.addEventListener("click", function () {
+    clearall()
+    ossenhuid_card.style.display = "flex";
 })
 
 
-mau__button.addEventListener("click", function () {
-    console.log("foo2");
-    popups.style.display = "flex"
-    mau.style.display = "flex"
-    amsterdam.style.display = ""
-    schip__card.style.display = ""
+
+const closeButton = document.getElementsByClassName("close-button");
+console.log(closeButton);
+
+closeButton.addEventListener("click", function () {
+    clearall;
 })
-
-
